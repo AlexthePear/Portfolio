a7f6757cbfcc41fb57dba309bf71c3d7a4fe1837
Alexander Peras <128564378+AlexthePear@users.noreply.github.com>
2024-04-25 17:22:19 -0700

    message

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,6 +14,9 @@ export default function Hero() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        <p>
+          Ethan told me to post :(
+        </p>
       </div>
     </div>
   );
